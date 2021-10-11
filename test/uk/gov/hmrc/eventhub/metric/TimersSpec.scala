26f30297402b698298678c3de3f00a495594aceb
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2021-10-11 12:38:06 +0100

    TimersSpec

---
+++ test/uk/gov/hmrc/eventhub/metric/TimersSpec.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.eventhub.metric
+
+import org.mockito.IdiomaticMockito
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class TimersSpec extends AnyFlatSpec with Matchers with IdiomaticMockito {
+
+  behavior of "Timers.CompletedTimer.time"
+
+  it should "return the difference between an end and start timer" in {
+    val start = 2
+    val end = 10
+    Timers.CompletedTimer(start, end).time should be(end - start)
+  }
+}
