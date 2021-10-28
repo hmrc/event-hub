54a6e3c253b87c86716d6b2197c81c139881b18c
Chris Gamble <85483770+spifix-hmrc@users.noreply.github.com>
2021-10-28 08:59:32 +0100

    swap implementation of linked hash map to allow fast insertion and removal (constant time), add index to subscriber work item to improve lookup time (#43)

--- app/uk/gov/hmrc/eventhub/repository/SubscriberQueueRepository.scala
+++ app/uk/gov/hmrc/eventhub/repository/SubscriberQueueRepository.scala
@@ -16,11 +16,14 @@
 
 package uk.gov.hmrc.eventhub.repository
 
+import org.mongodb.scala.bson.BsonDocument
 import org.mongodb.scala.model.Filters.equal
+import org.mongodb.scala.model.Indexes.ascending
+import org.mongodb.scala.model.{IndexModel, IndexOptions, Indexes}
 import play.api.Configuration
 import uk.gov.hmrc.eventhub.config.{Subscriber, TopicName}
 import uk.gov.hmrc.eventhub.model.Event
-import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.{MongoComponent, MongoUtils}
 import uk.gov.hmrc.mongo.workitem.ProcessingStatus.PermanentlyFailed
 import uk.gov.hmrc.mongo.workitem.{ProcessingStatus, WorkItem, WorkItemFields, WorkItemRepository}
 
@@ -43,6 +46,13 @@ class SubscriberQueueRepository(
 
   override def now(): Instant =
     Instant.now()
+
+  lazy val augmentedIndexes: Seq[IndexModel] = Seq(
+    IndexModel(Indexes.ascending("item.eventId"), IndexOptions().background(true))
+  ) ++ indexes
+
+  override def ensureIndexes: Future[Seq[String]] =
+    MongoUtils.ensureIndexes(collection, augmentedIndexes, true)
 
   override val inProgressRetryAfter: Duration =
     configuration.underlying.getDuration("queue.retryAfter")
